b1fa7732da384b583cd0a877d85a9df102441a6a
jan0r <falk.julian@icloud.com>
2025-12-16 18:34:31 +0100

    Auto deploy

--- deploy.php
+++ deploy.php
@@ -1,12 +1,4 @@
 <?php
-// Simple deploy webhook
-$secret = getenv('DEPLOY_SECRET');
-$token = $_GET['token'] ?? '';
-
-if (!$secret || $token !== $secret) {
-    http_response_code(403);
-    die('Forbidden');
-}
-
+// Deploy webhook - GitHub calls this, it pulls the code
 chdir(__DIR__);
 echo shell_exec('git pull 2>&1');
