b717c761853a202201ebe4776f507bea38fab830
jan0r <falk.julian@icloud.com>
2025-12-29 09:39:17 +0100

    Monday, 29th December, 2025 @ 09:39 Uhr - Edited files: , index.php,

--- index.php
+++ index.php
@@ -365,6 +365,10 @@ if ($is_single_post_view && $single_post) {
             </ul>
         </div>
     </footer>
+
+    <!-- 100% privacy-first analytics -->
+    <script data-collect-dnt="true" async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+    <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif?collect-dnt=true" alt="" referrerpolicy="no-referrer-when-downgrade"/></noscript>
 </body>
 
 </html>
