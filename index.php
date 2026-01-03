671dd4b23dec9736634b29d0b8545d51d778ed84
jan0r <falk.julian@icloud.com>
2026-01-03 01:03:24 +0100

    Saturday, 03th January, 2026 @ 01:03 Uhr - Edited files: , index.php,

--- index.php
+++ index.php
@@ -181,7 +181,7 @@ if ($is_single_post_view && $single_post) {
                         <svg class="hero-link-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
                         </svg>
-                        X (Twitter)
+                        X.COM
                     </a>
                     <a href="https://unsplash.com/@julianfalk" target="_blank" rel="noopener noreferrer" class="hero-link">
                         <svg class="hero-link-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
